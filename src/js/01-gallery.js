a8f7949082d426d29383da90acae3616a9458a1e
Иван Чмиль <chmil.ivan.u@gmail.com>
2022-12-25 15:04:43 +0200

    1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+const gallery = document.querySelector(`.gallery`);
+
+let htmlString = ``;
+galleryItems.forEach(element => {
+  htmlString += `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+</a>`;
+});
+gallery.insertAdjacentHTML(`beforeend`, htmlString);
+const galleryBox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
