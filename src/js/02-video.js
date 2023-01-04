192550201aa18699961fbfee44fb4f11bafbe0df
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-01-04 18:17:10 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+const throttle = require("lodash.throttle");
+console.log(Player);
+localStorage.setItem('test', 'test');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', throttle(trackTime, 1000));
+function trackTime(e) {
+  localStorage.setItem( "videoplayer-current-time", JSON.stringify(e.seconds));
+}
+
+
+const videoplayerCurrentTime = localStorage.getItem('videoplayer-current-time');
+if (videoplayerCurrentTime) {
+    player.setCurrentTime(videoplayerCurrentTime)
+}
+  
